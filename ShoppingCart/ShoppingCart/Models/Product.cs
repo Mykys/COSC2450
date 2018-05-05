c16736dabd7f051887856732edbc7d91c3d7cae0
Vinhlequang <s3713211@rmit.edu.vn>
2018-05-05 17:22:56 +0700

    Product management system

---
+++ ShoppingCart/ShoppingCart/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public int CategoryID { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+    }
+}
