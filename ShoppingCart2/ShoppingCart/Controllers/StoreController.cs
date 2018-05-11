57fa7bb0b273d991f63dd773f8996a07aa6e071e
Vinhlequang <s3713211@rmit.edu.vn>
2018-05-11 13:10:24 +0700

    Add login and register functions

---
+++ ShoppingCart2/ShoppingCart/Controllers/StoreController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Controllers
+{
+    public class StoreController : Controller
+    {
+        ShoppingCartContext storeDB = new ShoppingCartContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            var categories = storeDB.Categories.ToList();
+
+            return View(categories);
+        }
+
+        // GET: /Store/Browse?category=Shoes
+        public ActionResult Browse(string category)
+        {
+            var categoryModel = storeDB.Categories.Include("Products")
+               .Single(c => c.Title == category);
+
+            return View(categoryModel);
+        }
+
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = storeDB.Products.Find(id);
+
+            return View(product);
+        }
+
+        // GET: /Store/GenreMenu
+        [ChildActionOnly]
+        public ActionResult CategoryMenu()
+        {
+            var categories = storeDB.Categories.ToList();
+            return PartialView(categories);
+        }
+    }
+}
