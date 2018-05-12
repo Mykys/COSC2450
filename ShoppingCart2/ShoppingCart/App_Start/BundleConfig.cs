2da65e368fe0926655c12933b0b90ead95b91ec7
Vinhlequang <s3713211@rmit.edu.vn>
2018-05-12 15:30:23 +0700

    Working on credit card validation

--- ShoppingCart2/ShoppingCart/App_Start/BundleConfig.cs
+++ ShoppingCart2/ShoppingCart/App_Start/BundleConfig.cs
@@ -12,6 +12,9 @@ namespace ShoppingCart
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.cardCreditValidator.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
